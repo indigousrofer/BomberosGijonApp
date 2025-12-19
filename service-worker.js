9dd74eda43cca071a23eba5455f7259a9bc01403
indigousrofer <indigous87@gmail.com>
2025-12-19 17:51:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bomberos-v21';
+const CACHE_NAME = 'bomberos-v22';
 const urlsToCache = [
   './',
   './index.html',
@@ -50,6 +50,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
