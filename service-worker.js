87363db2b9ba4af1da978ca1a8e9886155e248f3
indigousrofer <indigous87@gmail.com>
2026-01-18 22:48:59 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    ========================================================= */
 
 // 1) Sube este número SOLO cuando publiques una versión nueva
-const CACHE_NAME = 'bomberos-v93';
+const CACHE_NAME = 'bomberos-v95';
 
 // 2) Solo los “core assets” imprescindibles (app shell)
 // OJO: si tu app vive bajo /BomberosGijonApp/ usa rutas coherentes con eso.
